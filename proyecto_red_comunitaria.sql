94a1710328a365249754cb91eac2e1f16758a0ae
Katherin Yesenia Monroy Echeverry <katherinymonroye@gmail.com>
2024-10-28 19:54:28 -0500

    se carga la base de datos en la cual vamos a trabajar

---
+++ proyecto_red_comunitaria.sql
@@ -0,0 +1,192 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 29-10-2024 a las 01:50:47
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `proyecto_red_comunitaria`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `emprendedor`
+--
+
+CREATE TABLE `emprendedor` (
+  `id_emprendedor` int(10) NOT NULL,
+  `id_pais` int(10) DEFAULT NULL,
+  `Nombre` varchar(100) NOT NULL,
+  `Apellido` varchar(100) NOT NULL,
+  `edad` varchar(100) NOT NULL,
+  `genero` varchar(100) NOT NULL,
+  `profesion` varchar(100) NOT NULL,
+  `año_nacimiento` varchar(100) NOT NULL,
+  `mes_nacimiento` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `gestiondeinformacion`
+--
+
+CREATE TABLE `gestiondeinformacion` (
+  `id_Gestion` int(10) NOT NULL,
+  `id_usuario` int(11) DEFAULT NULL,
+  `id_proyecto` int(11) DEFAULT NULL,
+  `fecha_generacion` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pais`
+--
+
+CREATE TABLE `pais` (
+  `id_pais` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `estado` varchar(100) NOT NULL,
+  `poblacion` varchar(100) NOT NULL,
+  `ciudad` varchar(100) NOT NULL,
+  `region` varchar(100) NOT NULL,
+  `latitud` varchar(100) NOT NULL,
+  `longitud` varchar(100) NOT NULL,
+  `calificacionriesgo` varchar(100) NOT NULL,
+  `tasaImpuesto` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proyecto`
+--
+
+CREATE TABLE `proyecto` (
+  `id_proyecto` int(10) NOT NULL,
+  `id_emprendedor` int(10) DEFAULT NULL,
+  `Nombre` varchar(100) NOT NULL,
+  `sector_industrial` varchar(100) NOT NULL,
+  `avaluo_proyecto` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id_Usuario` int(10) NOT NULL,
+  `nombre_Usuario` varchar(100) NOT NULL,
+  `contraseña` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `emprendedor`
+--
+ALTER TABLE `emprendedor`
+  ADD PRIMARY KEY (`id_emprendedor`),
+  ADD KEY `id_pais` (`id_pais`);
+
+--
+-- Indices de la tabla `gestiondeinformacion`
+--
+ALTER TABLE `gestiondeinformacion`
+  ADD PRIMARY KEY (`id_Gestion`),
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_proyecto` (`id_proyecto`);
+
+--
+-- Indices de la tabla `pais`
+--
+ALTER TABLE `pais`
+  ADD PRIMARY KEY (`id_pais`);
+
+--
+-- Indices de la tabla `proyecto`
+--
+ALTER TABLE `proyecto`
+  ADD PRIMARY KEY (`id_proyecto`),
+  ADD KEY `id_emprendedor` (`id_emprendedor`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_Usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `emprendedor`
+--
+ALTER TABLE `emprendedor`
+  MODIFY `id_emprendedor` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `gestiondeinformacion`
+--
+ALTER TABLE `gestiondeinformacion`
+  MODIFY `id_Gestion` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `proyecto`
+--
+ALTER TABLE `proyecto`
+  MODIFY `id_proyecto` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_Usuario` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `emprendedor`
+--
+ALTER TABLE `emprendedor`
+  ADD CONSTRAINT `emprendedor_ibfk_1` FOREIGN KEY (`id_pais`) REFERENCES `pais` (`id_Pais`);
+
+--
+-- Filtros para la tabla `gestiondeinformacion`
+--
+ALTER TABLE `gestiondeinformacion`
+  ADD CONSTRAINT `gestiondeinformacion_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_Usuario`),
+  ADD CONSTRAINT `gestiondeinformacion_ibfk_2` FOREIGN KEY (`id_proyecto`) REFERENCES `proyecto` (`id_proyecto`);
+
+--
+-- Filtros para la tabla `proyecto`
+--
+ALTER TABLE `proyecto`
+  ADD CONSTRAINT `proyecto_ibfk_1` FOREIGN KEY (`id_emprendedor`) REFERENCES `emprendedor` (`id_emprendedor`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
